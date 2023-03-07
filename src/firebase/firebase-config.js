70a3a9b3fe129bb7c633e42839b820dc6af3cdae
lucasdelmonte <delmontelucas678@gmail.com>
2023-03-06 21:45:08 -0300

    changed firebase configuration

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,18 +3,18 @@ import 'firebase/firestore';
 import 'firebase/auth';
 import { initializeApp } from "firebase/app";
 
-
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBw7czHxYX05smsZIC8i1wpcjVyfaN5QxI",
-  authDomain: "mcga-final-8f54f.firebaseapp.com",
-  projectId: "mcga-final-8f54f",
-  storageBucket: "mcga-final-8f54f.appspot.com",
-  messagingSenderId: "316298610716",
-  appId: "1:316298610716:web:4a9ac3dd77d55c8a21b7d9"
+    apiKey: "AIzaSyBw7czHxYX05smsZIC8i1wpcjVyfaN5QxI",
+    authDomain: "mcga-final-8f54f.firebaseapp.com",
+    projectId: "mcga-final-8f54f",
+    storageBucket: "mcga-final-8f54f.appspot.com",
+    messagingSenderId: "316298610716",
+    appId: "1:316298610716:web:4a9ac3dd77d55c8a21b7d9"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const FirebaseApp = initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.googleAuthProvider();
